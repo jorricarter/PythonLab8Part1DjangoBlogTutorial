b91f71834b87723cf75859c55395aa2d42e1ed44
Jack <jackxthexrabbit@gmail.com>
2018-04-08 05:39:38 -0500

    Testing some things to help with the project.

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # This tells the site to include the 'blog' app
+    # This tells the site to include the 'blog.html' app
     'blog',
 ]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -11,11 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog.html/', include('blog.html.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # the r means that anything inside the quotes is not meant to be python code
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', include('blog.urls')),
 ]
